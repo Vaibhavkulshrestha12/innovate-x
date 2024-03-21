c0e026d4ea64b86e611f33656d5b9971e9e77018
Vaibhavkulshrestha12 <vaibhavkulshrestha55@gmail.com>
2024-03-21 10:51:59 +0530

    g

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var registerBtn = document.getElementById("registerBtn");
+    if (registerBtn) {
+        registerBtn.addEventListener("click", function() {
+            window.location.href = ""; // Replace with your registration page URL
+        });
+    }
+});
